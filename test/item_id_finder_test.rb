1cfb5357c39ad6361c1df23e38586156df3dfea1
jonesdeini <jonesdeini@gmail.com>
2017-03-19 19:19:09 -0400

    Item id finder
    At this point in time we are considering the keyword supplied to be
    unique. However, we still return all matching results for the keyword.
    We considered simply calling first within ItemIdFinder but in
    this early stage of development we felt the best path was to not
    constrain ourselves.

--- test/item_id_finder_test.rb
+++ test/item_id_finder_test.rb
@@ -1,17 +1,25 @@
 require_relative "helper"
-#require_relative "../lib/dogedeini/item_id_finder"
+require_relative "../lib/dogedeini/item_id_finder"
 
 class ItemIdFinderTest < MiniTest::Unit::TestCase
   def mobile_stock_fixture
     File.open(File.expand_path("test/support/mobile_stock.json"), "r").read
   end
 
-  def test_derp
-    subject = ItemFinder.run!(mobile_stock_fixture, "doge")
+  def test_find_id_when_given_unique_keyword
+    subject = ItemFinder.run!(mobile_stock_fixture, "9001")
 
     results = subject.map{|x| x['id'] }.uniq
 
     assert_equal(results.size, 1)
     assert_equal(results.first, 1026)
+  end
+
+  def test_find_2_ids_when_given_nonunique_keyword
+    subject = ItemFinder.run!(mobile_stock_fixture, "doge")
+
+    results = subject.map{|x| x['id'] }.uniq
+
+    assert_equal(results.size, 2)
   end
 end
